8d2b2d321d2fa9950a5c79092957fafc0cbb0f85
Luke <jlukecarlson@gmail.com>
2012-10-20 16:13:13 -0400

    added the rest of the starting files

---
+++ president-game.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    var presidents = [['George Washington', 'http://upload.wikimedia.org/wikipedia/commons/1/12/Gilbert_Stuart%2C_George_Washington_%28Lansdowne_portrait%2C_1796%29.jpg'],['John Adams','http://upload.wikimedia.org/wikipedia/commons/1/17/Adamstrumbull.jpg']];
+});
