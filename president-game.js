d41c89fa69f28a92f5fb2b784d5c557c617910e7
Luke <jlukecarlson@gmail.com>
2012-10-20 16:25:11 -0400

    started presidents array

--- president-game.js
+++ president-game.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    var presidents = [['George Washington', 'http://upload.wikimedia.org/wikipedia/commons/1/12/Gilbert_Stuart%2C_George_Washington_%28Lansdowne_portrait%2C_1796%29.jpg'],['John Adams','http://upload.wikimedia.org/wikipedia/commons/1/17/Adamstrumbull.jpg']];
+    var presidents = [
+	['George Washington', 'http://upload.wikimedia.org/wikipedia/commons/1/12/Gilbert_Stuart%2C_George_Washington_%28Lansdowne_portrait%2C_1796%29.jpg'],
+	['John Adams','http://upload.wikimedia.org/wikipedia/commons/2/25/US_Navy_031029-N-6236G-001_A_painting_of_President_John_Adams_%281735-1826%29%2C_2nd_president_of_the_United_States%2C_by_Asher_B._Durand_%281767-1845%29-crop.jpg'],
+	['Thomas Jefferson','http://upload.wikimedia.org/wikipedia/commons/1/1e/Thomas_Jefferson_by_Rembrandt_Peale%2C_1800.jpg']
+
+    ];
 });
