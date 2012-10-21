0d995f861198813f7f2bd82867be836710f39bd6
Luke <jlukecarlson@gmail.com>
2012-10-20 20:15:46 -0400

    working on the answers section

--- president_game/static/president-game.js
+++ president_game/static/president-game.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#part1").hide();
     $("#part2").hide();
-    console.log(Math.floor(Math.random() * 4 + 1));
+    var correct_president = "no one";
     var presidents = [
 	['George Washington', 'http://upload.wikimedia.org/wikipedia/commons/1/12/Gilbert_Stuart%2C_George_Washington_%28Lansdowne_portrait%2C_1796%29.jpg'],
 	['John Adams','http://upload.wikimedia.org/wikipedia/commons/2/25/US_Navy_031029-N-6236G-001_A_painting_of_President_John_Adams_%281735-1826%29%2C_2nd_president_of_the_United_States%2C_by_Asher_B._Durand_%281767-1845%29-crop.jpg'],
@@ -50,13 +50,13 @@ $(document).ready(function(){
     var chooseOption = function(correct_president_number){ 
 	console.log('questions');
 	correct_option = Math.floor(Math.random() * 3);
-	$(".option" + correct_option).html(presidents[president_number][0]);
-	for (var i=0; i<5; i++){
+	$("#option" + correct_option).html(presidents[president_number][0]);
+	for (var i=1; i<5; i++){
 	    president_option = Math.floor(Math.random() * 43);
 	    console.log("option is" + president_option);
 	    if(president_option != correct_president_number){
 		if(i != correct_option){
-		    $(".option" + i).html(presidents[president_option][0]);	
+		    $("#option" + i).html(presidents[president_option][0]);	
 		}
 	    }
 	}
@@ -69,6 +69,7 @@ $(document).ready(function(){
 	});
 	president_number = Math.floor(Math.random() * 43);
 	console.log(presidents[president_number][0]);
+	correct_president = presidents[president_number][0];
 	$('#pic').html("<img id='president_pic' src='" + presidents[president_number][1] + "'>");
 	$('#president_pic').load(function() {
 	    console.log('loaded');
@@ -78,5 +79,12 @@ $(document).ready(function(){
 
     $('#start').click(function () {
 	startGame();
+    });
+    $('.choice').click(function(){
+	if($('.choice').text() = correct_president){
+	    $('#answer').html('correct');
+	} else {
+	     $('#answer').html('wrong');
+	}
     });
 });
