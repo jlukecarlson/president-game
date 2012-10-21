83a707e98781ecc95a1cba71804fcc6a898d290a
Luke <jlukecarlson@gmail.com>
2012-10-20 20:02:42 -0400

    added the president options and made them random and unique

--- president_game/static/president-game.js
+++ president_game/static/president-game.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#part1").hide();
     $("#part2").hide();
+    console.log(Math.floor(Math.random() * 4 + 1));
     var presidents = [
 	['George Washington', 'http://upload.wikimedia.org/wikipedia/commons/1/12/Gilbert_Stuart%2C_George_Washington_%28Lansdowne_portrait%2C_1796%29.jpg'],
 	['John Adams','http://upload.wikimedia.org/wikipedia/commons/2/25/US_Navy_031029-N-6236G-001_A_painting_of_President_John_Adams_%281735-1826%29%2C_2nd_president_of_the_United_States%2C_by_Asher_B._Durand_%281767-1845%29-crop.jpg'],
@@ -46,8 +47,21 @@ $(document).ready(function(){
 	['George W. Bush','http://upload.wikimedia.org/wikipedia/commons/d/d4/George-W-Bush.jpeg'],
 	['Barack Obama','http://upload.wikimedia.org/wikipedia/commons/e/e9/Official_portrait_of_Barack_Obama.jpg']
     ];
-    var startCountdown = function(){ 
-	setTimeout(function(){$('#part2').fadeIn();},3000);
+    var chooseOption = function(correct_president_number){ 
+	console.log('questions');
+	correct_option = Math.floor(Math.random() * 3);
+	$(".option" + correct_option).html(presidents[president_number][0]);
+	for (var i=0; i<5; i++){
+	    president_option = Math.floor(Math.random() * 43);
+	    console.log("option is" + president_option);
+	    if(president_option != correct_president_number){
+		if(i != correct_option){
+		    $(".option" + i).html(presidents[president_option][0]);	
+		}
+	    }
+	}
+	$('#part2').fadeIn();
+	//setTimeout(function(){$('#part2').fadeIn();},3000);
     }
     var startGame = function(){
 	$('#welcome').fadeOut(function(){
@@ -55,9 +69,10 @@ $(document).ready(function(){
 	});
 	president_number = Math.floor(Math.random() * 43);
 	console.log(presidents[president_number][0]);
-	$('#pic').html("<img src='" + presidents[president_number][1] + "'>");
-	$('#pic').load(function() {
-	  setTimeout(function(){$('#part2').fadeIn()},3000);
+	$('#pic').html("<img id='president_pic' src='" + presidents[president_number][1] + "'>");
+	$('#president_pic').load(function() {
+	    console.log('loaded');
+	    setTimeout(function(){$('#part1').fadeOut(chooseOption(president_number))},3000);
 	});
     }
 
